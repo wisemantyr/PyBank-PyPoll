5c3de2633982d2204e5e7212c7fb70ff34deccdd
wisemantyr <wisemantyr@gmail.com>
2020-02-22 17:40:11 -0500

    Created dictionary with unique candidates as keys and lists of candidates' votes as their values. Cleaned markup. Changed file path to satisfy GitHub maximum file size recommendation

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,59 +1,72 @@
+'''
 #PYPOLL
 #Your task is to create a Python script that analyzes the votes and calculates each of the following:
 '''
-open csv file
-create csv reader
-skip the header
-'''
+#import modules
 import os
 import csv
-pypoll_csvpath = os.path.join("Resources", "election_data.csv")
 
+#define file path
+pypoll_csvpath = os.path.join("..","..","..", "UR-RICH-DATA-PT-01-2020-U-C","03-Python","HW",
+"Instructions","PyPoll","Resources", "election_data.csv")
+
+#create empty lists and dictionaries
 AllVotes = []
 AllCandidates = []
 CandidateVotes = {}
+CandidatePercentages = {}
 
-
+#open csv, create reader, skip header
 with open(pypoll_csvpath, newline="") as PyPollCsv:
     pypoll_csvreader = csv.reader(PyPollCsv, delimiter=",")
     pypoll_header = next(pypoll_csvreader)
-      
-    #The total number of votes cast
     '''
-    sum of index (0)
+    The total number of votes cast
     '''
+    #search file
     for row in pypoll_csvreader:
+        #append lists
         AllVotes.append(int(row[0]))
         AllCandidates.append(row[2])
         
-
+    #sum the votes
     TotalVotes = sum(AllVotes)
     print(TotalVotes)
-    UniqueCandidates = list(set(AllCandidates))
-    print(UniqueCandidates)
+    
+    '''
     #A complete list of candidates who received votes
     '''
-    list of index(2) unique values
+
+    #create list of unique candidates
+    UniqueCandidates = list(set(AllCandidates))
+    print(UniqueCandidates)
+
     '''
-    
-
     #The percentage of votes each candidate won
-    #search data for candidate
-
-        
-    #find all corrosponding index (0) Values
-    #sum them
-    #create a dictionary of candidates and sums 
-    #divide sum by the total votes cast
-    #create another dictionary of candidates and percentages
-    #format as a percent
-
-
+    '''
+    #reset csvreader and skip header to search data again
+    PyPollCsv.seek(0)
+    next(pypoll_csvreader)
+  
+    #search data
+    for index in pypoll_csvreader:
+        #iterate through unique candidates
+        for candidate in UniqueCandidates:
+            #create a dictionary with each unique candidate as a key
+            CandidateVotes.setdefault(candidate, [])
+            #when unique candidate key is found in the data
+            if index[2] == candidate:
+                #append the associated value list with the votes
+                CandidateVotes[candidate].append(int(index[0]))
+ 
+    '''
     #The total number of votes each candidate won
-    '''found above'''
+    '''
 
+    '''
     #The winner of the election based on popular vote.
     '''
-    print candidate with highest number
+    
     '''
-    #In addition, your final script should both print the analysis to the terminal and export a text file with the results.
+    In addition, your final script should both print the analysis to the terminal and export a text file with the results.
+    '''
