bfc8fb7c6a28d5520c8af8baefecfb5caed469f9
wisemantyr <wisemantyr@gmail.com>
2020-02-22 20:52:44 -0500

    Final Code

--- PyBank/main.py
+++ PyBank/main.py
@@ -9,7 +9,6 @@ import csv
 pybank_csvpath = os.path.join("Resources", "budget_data.csv")
 
 #open CSV, create reader object, skip header
-
 with open (pybank_csvpath, newline="") as PyBankCsv:
     pybank_csvreader = csv.reader(PyBankCsv, delimiter = ",")
     pybank_header = next(pybank_csvreader)
@@ -17,27 +16,26 @@ with open (pybank_csvpath, newline="") as PyBankCsv:
     for row in pybank_csvreader:
         #fill list with months
         Months.append(row[0])
-                
       
         #fill list with profits
         Profits.append(int(row[1]))
-                
+
+    #calculate number of Months and TotalProfit            
     TotalMonths = len(Months)
     TotalProfit = sum(Profits)
+    FormattedTotalProfit = "$%.2f" % TotalProfit
     
     #create tuple pairs of each number in profits and the number after it
     #subtract first value from second value to calculate change
     #fill list with values
     ProfitChanges = [second - first for first,second in zip(Profits, Profits[1:])]
 
-
     #calculate average of list
     TotalChange = sum(ProfitChanges)
     AverageChange = round((TotalChange/(len(ProfitChanges))), 2)
     
-    #add value to beginning of list for accurate indexes
+    #add value to beginning of Profit Changes to make length same as Months list for accurate indexing
     ProfitChanges.insert(0,0)
-    str("${:,.2f}".format(*ProfitChanges))
     
     #find the largest number in change list and its index
     GreatestIncrease = max(ProfitChanges)
@@ -47,12 +45,15 @@ with open (pybank_csvpath, newline="") as PyBankCsv:
     GreatestDecrease = min(ProfitChanges)
     GreatestDecreaseIndex = ProfitChanges.index(GreatestDecrease)
 
-    #create list filled with tuples of months and changes formtted as dollars
-    CombinedList = list(zip(Months, ProfitChanges))
+    #format Profit Changes list items
+    FormattedProfitChanges = ["$%.2f" % item for item in ProfitChanges]
 
-    #save results as lines
+    #create list filled with tuples of months and changes formtted as dollars
+    CombinedList = list(zip(Months, FormattedProfitChanges))
+
+    #save results as lines for export
     Line1 = "Total Months: " + str(TotalMonths)
-    Line2 = "Total: $" + str(TotalProfit)
+    Line2 = "Total: $" + FormattedTotalProfit
     Line3 = "Average Change: $" + str(AverageChange)
     Line4 = "Greatest Increase in Profits: " + str(CombinedList[GreatestIncreaseIndex])
     Line5 = "Greatest Decrease in Profits: " + str(CombinedList[GreatestDecreaseIndex])
